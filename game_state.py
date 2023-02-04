ded2a26bb2d229cdef5e93e3d9a21fffeb08ca74
rentacow <33181319+rentacow@users.noreply.github.com>
2023-02-04 05:33:55 -0800

    Create game_state.py
    Proposed implementation for game state tracking.

---
+++ game_state.py
@@ -0,0 +1,34 @@
+# proposed game state tracking implementation
+
+import openai
+
+def level_up(completion):
+    # Define the keyword that indicates the player has leveled up
+    keyword = "level up"
+
+    # Check if the keyword appears in the completion
+    if keyword in completion:
+        # Split the completion into words
+        words = completion.split()
+
+        # Check if the keyword was not in the completion request
+        request = openai.Completion.request
+        if keyword not in request.split():
+            # If the keyword was not in the request, the player has leveled up
+            print("Level up!")
+        else:
+            print("Keyword was in the request, no level up.")
+    else:
+        print("Keyword not found, no level up.")
+
+# Example usage
+completion = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt="How can I reach the next level in my game?",
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+level_up(completion.choices[0].text)
