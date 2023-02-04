757bc08dd85be840f2c728a1a9ad481c156734f0
rentacow <renatcow@gmail.com>
2023-02-04 01:51:57 -0800

    Good luck talking your way out of this one!

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # AI Chat Bot is Detective, User is Suspect
 
-######   NOT IMPLIMENTED YET   ########
+######   NOT IMPLEMENTED YET   ########
 # Level 1: easy (level up w/ free to go)
 # level 2: hard (this version, level up w/ we're done OR lawyer arrives to dodge poly)
 # level 3: polygraph examination (lol)
